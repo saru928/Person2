83d188eb904204dab5e82e42fe523b51c55609aa
saru928 <kana.softball5@gmail.com>
2024-08-01 20:55:19 +0900

    ensyu5

--- src/Test.java
+++ src/Test.java
@@ -11,5 +11,17 @@ public class Test {
 		Person jiro = new Person("jiro",20);
 		System.out.println(jiro.name);
 		System.out.println(jiro.age);
+		
+		Person saburo = new Person("saburo");
+		System.out.println(saburo.name);
+		System.out.println(saburo.age);
+		
+		Person nanasi = new Person(25);
+		System.out.println(nanasi.name);
+		System.out.println(nanasi.age);
+		
+		Person hanako = new Person(17,"hanako");
+		System.out.println(hanako.name);
+		System.out.println(hanako.age);
 	}
 }
